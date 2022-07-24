8cce04c2f12c078d2a9c95694b9056844ea1986b
Kapusnik, Michal <mkapusnik@netsuite.com>
2022-07-24 23:29:10 +0200

    Camera - improve logging

--- camera/nsd/main.py
+++ camera/nsd/main.py
@@ -18,10 +18,10 @@ info = ServiceInfo(
 )
 
 zeroconf = Zeroconf(ip_version=IPVersion.V4Only)
-print("Registration of a service {} on {}:{}, press Ctrl-C to exit...".format(name, server, port))
+print("Registration of a service {} on {}:{}".format(name, server, port))
 zeroconf.register_service(info)
 try:
-    input("Press enter to exit...\n\n")
+    input("Press any key to exit...\n\n")
 finally:
     print("Unregistering...")
     zeroconf.unregister_service(info)
