0060a0edbd24be99867277ce5cf8289a224678f8
Kapusnik, Michal <michal.kapusnik@oracle.com>
2022-07-28 09:47:34 +0200

    Camera - base for allinone script

---
+++ camera/all_in_one/main.py
@@ -0,0 +1,28 @@
+from zeroconf import IPVersion, ServiceInfo, Zeroconf
+import os
+
+hostFile = open("/hostname", "r")
+server = hostFile.readline().strip()
+
+port = int(os.environ.get('PORT', 8555))
+name = os.getenv('NAME', server.title())
+desc = {'deviceName': name}
+type = '_nanny._tcp.local.'
+
+info = ServiceInfo(
+    type,
+    name+"."+type,
+    port=port,
+    properties=desc,
+    server=server+".local.",
+)
+
+zeroconf = Zeroconf(ip_version=IPVersion.V4Only)
+print("Registration of a service {} on {}:{}".format(name, server, port))
+zeroconf.register_service(info)
+try:
+    input("Press any key to exit...\n\n")
+finally:
+    print("Unregistering...")
+    zeroconf.unregister_service(info)
+    zeroconf.close()
